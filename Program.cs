284768094961ee15ac42fcd564dccf5757fe561a
Kevin Griffin <kevin@kevgriffin.com>
2019-10-31 11:58:44 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwiftKick.DapperDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
