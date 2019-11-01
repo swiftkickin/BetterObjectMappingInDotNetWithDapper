af214a8059fad94c056e245a9b44f7166a69d95b
Kevin Griffin <kevin@kevgriffin.com>
2019-11-01 16:00:36 -0400

    Demos!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,261 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
 
 namespace SwiftKick.DapperDemos
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sqlConnectionString = "Server=.;Database=GriffCoUnlimited;Trusted_Connection=True;";
+
+            // DEMO 1: QueryAsync<> 
+            //await DemoWithQueryAsync(sqlConnectionString);
+
+            // DEMO 2: QueryAsync<> with Parameters
+            //await DemoWithQueryAsyncWithParameters(sqlConnectionString);
+            //await DemoWithQueryAsyncWithDynamicParameters(sqlConnectionString);
+
+            // DEMO 3: QueryFirst / QueryFirstOrDefault
+            //await DemoWithQueryFirst(sqlConnectionString);
+
+            // DEMO 4: INSERT/UPDATE/DELETE with ExecuteAsync
+            //await DemoExecuteAsyncWithInsert(sqlConnectionString);
+            //await DemoExecuteAsyncWithUpdate(sqlConnectionString);
+            //await DemoExecuteAsyncWithDelete(sqlConnectionString);
+
+            // DEMO 5: Executing stored procedures
+            //await DemoStoredProcedure(sqlConnectionString);
+
+            // DEMO 6: Working with Transactions
+            //await DemoTransactions(sqlConnectionString);
+
+            // DEMO 7: Dynamic results
+            await DemoDynamicResults(sqlConnectionString);
+
+            // DEMO 8: Mapping 
+            // DEMO 9: Multi-mapping
+            // DEMO 10: Multi-results
+        }
+
+        #region Helper methods
+        private static void WriteList(IEnumerable<User> result)
+        {
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName}\t\t{r.LastName}\t\t{r.DateOfBirth}");
+            }
+        }
+
+        private static void WriteList(IEnumerable<dynamic> result)
+        {
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.FirstName}\t\t{r.LastName}\t\t{r.DateOfBirth}");
+            }
+        }
+
+        private static void WriteList(User result)
+        {
+            Console.WriteLine($"{result.FirstName}\t\t{result.LastName}\t\t{result.DateOfBirth}");
+        }
+#endregion
+
+        private static async Task DemoWithQueryAsync(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"SELECT [Id]
+                              ,[FirstName]
+                              ,[LastName]
+                              ,[PhoneNumber]
+                              ,[UserName]
+                              ,[DateOfBirth]
+                              ,[City]
+                              ,[State]
+                              ,[ZipCode]
+                          FROM [dbo].[MassiveUserList]";
+            var result = await connection.QueryAsync<User>(sql);
+            WriteList(result);
+        }
+
+        private static async Task DemoWithQueryAsyncWithParameters(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"SELECT [Id]
+                              ,[FirstName]
+                              ,[LastName]
+                              ,[PhoneNumber]
+                              ,[UserName]
+                              ,[DateOfBirth]
+                              ,[City]
+                              ,[State]
+                              ,[ZipCode]
+                          FROM [dbo].[MassiveUserList]
+                          WHERE DateOfBirth > @dateOfBirth";
+
+            var result = await connection.QueryAsync<User>(sql,
+                new { dateOfBirth = new DateTime(2005, 10, 01) });
+            WriteList(result);
+        }
+
+        private static async Task DemoWithQueryAsyncWithDynamicParameters(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"SELECT [Id]
+                              ,[FirstName]
+                              ,[LastName]
+                              ,[PhoneNumber]
+                              ,[UserName]
+                              ,[DateOfBirth]
+                              ,[City]
+                              ,[State]
+                              ,[ZipCode]
+                          FROM [dbo].[MassiveUserList]
+                          WHERE DateOfBirth > @dateOfBirth";
+
+            var dynamicParam = new DynamicParameters();
+            dynamicParam.Add("dateOfBirth", new DateTime(2005, 10, 01));
+
+            var result = await connection.QueryAsync<User>(sql, dynamicParam);
+
+            WriteList(result);
+        }
+
+        private static async Task DemoWithQueryFirst(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"SELECT TOP 1 [Id]
+                              ,[FirstName]
+                              ,[LastName]
+                              ,[PhoneNumber]
+                              ,[UserName]
+                              ,[DateOfBirth]
+                              ,[City]
+                              ,[State]
+                              ,[ZipCode]
+                          FROM [dbo].[MassiveUserList]";
+
+            var result = await connection.QueryFirstAsync<User>(sql);
+            WriteList(result);
+        }
+
+        private static async Task DemoExecuteAsyncWithInsert(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"INSERT INTO [dbo].[MassiveUserList]
+                               ([FirstName]
+                               ,[LastName]
+                               ,[PhoneNumber]
+                               ,[UserName]
+                               ,[DateOfBirth]
+                               ,[City]
+                               ,[State]
+                               ,[ZipCode])
+                         VALUES
+                               (@firstName, @lastName, @phoneNumber, 
+                                @userName, @dateOfBirth,
+                                @city, @state, @zipCode)";
+
+            var newUser = new User()
+            {
+                FirstName = "Jim", LastName = "Bob", City = "Chesapeake", State = "Virginia", ZipCode = "23320",
+                DateOfBirth = DateTime.Parse("1999-01-01"),
+                PhoneNumber = "757-867-5309", UserName = "COMPUTER/JimBob"
+            };
+        
+            var result = await connection.ExecuteAsync(sql, newUser);
+        }
+
+        private static async Task DemoExecuteAsyncWithUpdate(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"UPDATE [dbo].[MassiveUserList]
+                        SET DateOfBirth = @dateOfBirth
+                        WHERE FirstName = @firstName";
+
+            var result = await connection.ExecuteAsync(sql,
+                new {dateOfBirth = DateTime.Parse("2009-01-01"), firstName = "Jim"});
+        }
+
+        private static async Task DemoExecuteAsyncWithDelete(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"DELETE FROM [dbo].[MassiveUserList]                        
+                        WHERE LastName = @lastName";
+
+            var result = await connection.ExecuteAsync(sql, new {lastName = "Bob"});
+        }
+
+
+        private static async Task DemoStoredProcedure(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = "dbo.LastNameList";
+
+            var results = await connection.QueryAsync<string>(sql, commandType: CommandType.StoredProcedure);
+        }
+
+        private static async Task DemoTransactions(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            await connection.OpenAsync();
+            await using var transaction = connection.BeginTransaction();
+
+            var sql = @"INSERT INTO [dbo].[MassiveUserList]
+                               ([FirstName]
+                               ,[LastName]
+                               ,[PhoneNumber]
+                               ,[UserName]
+                               ,[DateOfBirth]
+                               ,[City]
+                               ,[State]
+                               ,[ZipCode])
+                         VALUES
+                               (@firstName, @lastName, @phoneNumber, 
+                                @userName, @dateOfBirth,
+                                @city, @state, @zipCode)";
+
+            var newUser = new User()
+            {
+                FirstName = "Jim",
+                LastName = "Bob",
+                City = "Chesapeake",
+                State = "Virginia",
+                ZipCode = "23320",
+                DateOfBirth = DateTime.Parse("1999-01-01"),
+                PhoneNumber = "757-867-5309",
+                UserName = "COMPUTER/JimBob"
+            };
+
+            for (var x = 0; x < 10000; x++)
+            {
+                var result = await connection.ExecuteAsync(sql, newUser, transaction);
+                newUser.DateOfBirth = newUser.DateOfBirth.AddDays(1);
+            }
+
+            transaction.Commit();
+        }
+
+        private static async Task DemoDynamicResults(string sqlConnectionString)
+        {
+            await using var connection = new SqlConnection(sqlConnectionString);
+            var sql = @"SELECT [Id]
+                              ,[FirstName]
+                              ,[LastName]
+                              ,[PhoneNumber]
+                              ,[UserName]
+                              ,[DateOfBirth]
+                              ,[City]
+                              ,[State]
+                              ,[ZipCode]
+                          FROM [dbo].[MassiveUserList]";
+
+            var result = await connection.QueryAsync(sql);
+            WriteList(result);
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwiftKick.DapperDemos
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
